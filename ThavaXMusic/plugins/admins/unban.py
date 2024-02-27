4da23fe960676dce5b57dfb87c69517c020877f8
Thava-OX <159293177+Thava-OX@users.noreply.github.com>
2024-02-27 19:48:11 +0530

    Create unban.py

---
+++ ThavaXMusic/plugins/admins/unban.py
@@ -0,0 +1,89 @@
+from pyrogram import filters, enums
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ChatPermissions
+)
+from pyrogram.errors.exceptions.bad_request_400 import (
+    ChatAdminRequired,
+    UserAdminInvalid,
+    BadRequest
+)
+
+import datetime
+from ThavaXMusic import app
+
+def mention(user, name, mention=True):
+    if mention == True:
+        link = f"[{name}](tg://openmessage?user_id={user})"
+    else:
+        link = f"[{name}](https://t.me/{user})"
+    return link
+
+async def get_userid_from_username(username):
+    try:
+        user = await app.get_users(username)
+    except:
+        return None
+    
+    user_obj = [user.id, user.first_name]
+    return user_obj
+
+async def unban_user(user_id, first_name, admin_id, admin_name, chat_id):
+    try:
+        await app.unban_chat_member(chat_id, user_id)
+    except ChatAdminRequired:
+        msg_text = "Ban rights? Nah, I'm just here for the digital high-fives 🙌\nGive me ban rights! 😡🥺"
+        return msg_text
+    except Exception as e:
+        msg_text = f"opps!!\n{e}"
+        return msg_text
+
+    user_mention = mention(user_id, first_name)
+    admin_mention = mention(admin_id, admin_name)
+    
+    msg_text = f"{user_mention} was unbanned by {admin_mention}"
+    return msg_text
+
+@app.on_message(filters.command(["unban"]))
+async def unban_command_handler(client, message):
+    chat = message.chat
+    chat_id = chat.id
+    admin_id = message.from_user.id
+    admin_name = message.from_user.first_name
+    member = await chat.get_member(admin_id)
+    if member.status == enums.ChatMemberStatus.ADMINISTRATOR or member.status == enums.ChatMemberStatus.OWNER:
+        if member.privileges.can_restrict_members:
+            pass
+        else:
+            msg_text = "You dont have permission to unban someone"
+            return await message.reply_text(msg_text)
+    else:
+        msg_text = "You dont have permission to unban someone"
+        return await message.reply_text(msg_text)
+
+    # Extract the user ID from the command or reply
+    if len(message.command) > 1:
+        try:
+            user_id = int(message.command[1])
+            first_name = "User"
+        except:
+            user_obj = await get_userid_from_username(message.command[1])
+            if user_obj == None:
+                    return await message.reply_text("I can't find that user")
+            user_id = user_obj[0]
+            first_name = user_obj[1]
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        first_name = message.reply_to_message.from_user.first_name
+    else:
+        await message.reply_text("Please specify a valid user or reply to that user's message")
+        return
+        
+    msg_text = await unban_user(user_id, first_name, admin_id, admin_name, chat_id)
+    await message.reply_text(msg_text)
+
+
+
+
